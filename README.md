51a1fd9a8ff088846a316a387605d93071930e3b
gokulkumar <gokulkumarofficial@outlook.com>
2024-09-07 16:42:38 +0530

    13th problem solved

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Java8Problems
 
 12) Java 8 program to check if two strings are anagrams or not?
+13) Find sum of all digits of a number in Java 8?
--- src/main/java/com/advance/java8/problems/Java8Problems.java
+++ src/main/java/com/advance/java8/problems/Java8Problems.java
@@ -3,6 +3,7 @@ package com.advance.java8.problems;
 import java.util.Arrays;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Java8Problems {
     public static void main(String[] args) {
@@ -10,9 +11,25 @@ public class Java8Problems {
 
         //12.Java 8 program to check if two strings are anagrams or not?
         java.anagramOrNot();
+
+        //13.Find sum of all digits of a number in Java 8?
+        java.sumOfDigits(15623);
     }
 
-    public void anagramOrNot() {
+    private void sumOfDigits(int number) {
+        //normal
+        Integer sumOfDigits = Arrays.stream(String.valueOf(number).split(""))
+                .map(Integer::valueOf)
+                .reduce(0, Integer::sum);
+        System.out.println("sumOfDigits of "+ number+ ": "+sumOfDigits);
+
+        //optimized
+        Integer sum = Stream.of(String.valueOf(number).split(""))
+                .collect(Collectors.summingInt(Integer::parseInt));
+        System.out.println("sum of "+ number+ ": "+sum);
+    }
+
+    private void anagramOrNot() {
         BiFunction<String, String, Boolean> anagramsOrNot = (word1, word2) -> {
             String firstWord = Arrays.stream(word1.split(""))
                     .map(String::toLowerCase)
