b487c9f8f0256b89712650352e6a4ca1cd78d6db
gokulkumar <gokulkumarofficial@outlook.com>
2024-09-07 16:25:45 +0530

    12th problem solved

--- src/main/java/com/advance/java8/problems/Java8Problems.java
+++ src/main/java/com/advance/java8/problems/Java8Problems.java
@@ -1,7 +1,33 @@
 package com.advance.java8.problems;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
 public class Java8Problems {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Java8Problems java = new Java8Problems();
+
+        //12.Java 8 program to check if two strings are anagrams or not?
+        java.anagramOrNot();
+    }
+
+    public void anagramOrNot() {
+        BiFunction<String, String, Boolean> anagramsOrNot = (word1, word2) -> {
+            String firstWord = Arrays.stream(word1.split(""))
+                    .map(String::toLowerCase)
+                    .sorted()
+                    .collect(Collectors.joining());
+
+            String secondWord = Arrays.stream(word2.split(""))
+                    .map(String::toLowerCase)
+                    .sorted()
+                    .collect(Collectors.joining());
+
+            return firstWord.equalsIgnoreCase(secondWord);
+        };
+
+        Boolean result12 = anagramsOrNot.apply("RaceCar", "CarRace");
+        System.out.println("Anagram Or Not: " + result12);
     }
 }
