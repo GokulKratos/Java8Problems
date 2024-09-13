48d46aa8087228fdd8800364da13a7f8d7c510b6
gokulkumar <gokulkumarofficial@outlook.com>
2024-09-13 13:12:18 +0530

    completed all question in ems

--- README.md
+++ README.md
@@ -1,23 +1,47 @@
-# Java8Problems
+<details>
+  <summary>Java 8 Problems</summary>
 
-12) Java 8 program to check if two strings are anagrams or not?
-13) Find sum of all digits of a number in Java 8?
-14) Find second largest number in an integer array?
-15) Given a list of strings, sort them according to increasing order of their length?
-16) Given an integer array, find sum and average of all elements?
-17) How do you find common elements between two arrays?
-18) Reverse each word of a string using Java 8 streams?
-19) How do you find sum of first 10 natural numbers?
-20) Reverse an integer array?
-21) Print first 10 even numbers?
-22) How do you find the most repeated element in an array?
-23) Palindrome program using Java 8 streams?
-24) Given a list of strings, find out those strings which start with a number?
-25) How do you extract duplicate elements from an array?
-26) Print duplicate characters in a string?
-27) Find first repeated character in a string?
-28) Find first non-repeated character in a string?
-29) Fibonacci series?
-30) First 10 odd numbers?
-31) How do you get last element of an array?
-32) Find the age of a person in years if the birthday has given?
+12. Java 8 program to check if two strings are anagrams or not?
+13. Find sum of all digits of a number in Java 8?
+14. Find second largest number in an integer array?
+15. Given a list of strings, sort them according to increasing order of their length?
+16. Given an integer array, find sum and average of all elements?
+17. How do you find common elements between two arrays?
+18. Reverse each word of a string using Java 8 streams?
+19. How do you find sum of first 10 natural numbers?
+20. Reverse an integer array?
+21. Print first 10 even numbers?
+22. How do you find the most repeated element in an array?
+23. Palindrome program using Java 8 streams?
+24. Given a list of strings, find out those strings which start with a number?
+25. How do you extract duplicate elements from an array?
+26. Print duplicate characters in a string?
+27. Find first repeated character in a string?
+28. Find first non-repeated character in a string?
+29. Fibonacci series?
+30. First 10 odd numbers?
+31. How do you get last element of an array?
+32. Find the age of a person in years if the birthday is given?
+
+</details>
+
+<details>
+  <summary>Solving Real Time Queries Using Java 8 Features - Employee Management System</summary>
+
+1. How many male and female employees are there in the organization?
+2. Print the name of all departments in the organization?
+3. What is the average age of male and female employees?
+4. Get the details of the highest paid employee in the organization?
+5. Get the names of all employees who have joined after 2015?
+6. Count the number of employees in each department?
+7. What is the average salary of each department?
+8. Get the details of the youngest male employee in the product development department?
+9. Who has the most working experience in the organization?
+10. How many male and female employees are there in the sales and marketing team?
+11. What is the average salary of male and female employees?
+12. List down the names of all employees in each department?
+13. What is the average salary and total salary of the whole organization?
+14. Separate the employees who are younger or equal to 25 years from those employees who are older than 25 years?
+15. Who is the oldest employee in the organization? What is his age and which department does he belong to?
+
+</details>
--- src/main/java/com/advance/java8/ems/service/EMSservice.java
+++ src/main/java/com/advance/java8/ems/service/EMSservice.java
@@ -2,12 +2,10 @@ package com.advance.java8.ems.service;
 
 import com.advance.java8.ems.model.Employee;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class EMSservice {
 
@@ -23,6 +21,158 @@ public class EMSservice {
 
         //3.What is the average age of male and female employees?
         ems.avgAgeOfEmployees(employees);
+
+        //4.Get the details of highest paid employee in the organization?
+        ems.highestPaidEmployee(employees);
+
+        //5.Get the names of all employees who have joined after 2015?
+        ems.employeesAfterYear(employees);
+
+        //6.Count the number of employees in each department?
+        ems.countEmpDept(employees);
+
+        //7.What is the average salary of each department?
+        ems.avgSalByDept(employees);
+
+        //8.Get the details of youngest male employee in the product development department?
+        ems.youngestMaleEmpByDept(employees);
+
+        //9.Who has the most working experience in the organization?
+        ems.maxWorkExp(employees);
+
+        //10.How many male and female employees are there in the sales and marketing team?
+        ems.empByDept(employees);
+
+        //11.What is the average salary of male and female employees
+        ems.avgSalOfMaleAndFemaleEmployees(employees);
+
+        //13.List down the names of all employees in each department?
+        ems.nameOfEmpByDept(employees);
+
+        //14.What is the average salary and total salary of the whole organization?
+        ems.avgAndTotalSal(employees);
+
+        //15.Separate the employees who are younger or equal to 25 years from those employees who are older than 25 years?
+        ems.separateByAge(employees);
+
+        //16.Who is the oldest employee in the organization? What is his age and which department he belongs to?
+        ems.oldestEmp(employees);
+    }
+
+    private void oldestEmp(List<Employee> employees) {
+        Employee emp = employees.stream()
+                .max(Comparator.comparingInt(Employee::getAge))
+                .get();
+        System.out.println("16.oldest employee in the organization age and department: "+emp);
+    }
+
+    private void separateByAge(List<Employee> employees) {
+        Map<String, List<Employee>> emp = employees.stream()
+                .collect(Collectors.groupingBy(employee -> {
+                    if (employee.getAge() <= 25) {
+                        return "<=25 years";
+                    } else {
+                        return ">25years";
+                    }
+                }));
+        System.out.println("15.Employees who are younger or equal to 25 years from those employees who are older than 25 years" + emp);
+
+        //optimized
+        Map<Boolean, List<Employee>> empOpt = employees.stream()
+                .collect(Collectors.partitioningBy(employee -> employee.getAge() <=25 ));
+        System.out.println("15.Employees who are younger or equal to 25 years from those employees who are older than 25 years" + empOpt);
+    }
+
+    private void avgAndTotalSal(List<Employee> employees) {
+        Double avgSal = employees.stream()
+                .collect(Collectors.averagingDouble(Employee::getSalary));
+
+        Double totalSal = employees.stream()
+                .mapToDouble(Employee::getSalary)
+                .sum();
+        System.out.println("14.Average salary and total salary of the whole organization: " + avgSal + " | " + totalSal);
+
+        //optimized
+        DoubleSummaryStatistics dss = employees.stream()
+                .collect(Collectors.summarizingDouble(Employee::getSalary));
+        System.out.println("14.Optimized Average salary and total salary of the whole organization: " + dss.getAverage() + " | " + dss.getSum());
+    }
+
+    private void nameOfEmpByDept(List<Employee> employees) {
+        Map<String, List<String>> emp = employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().stream().map(Employee::getName).toList()));
+        System.out.println("13.Names of all employees in each department: " + emp);
+    }
+
+    private void avgSalOfMaleAndFemaleEmployees(List<Employee> employees) {
+        Map<String, Double> emp = employees.stream()
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingDouble(Employee::getSalary)));
+        System.out.println("11.Average salary of male and female employees: " + emp);
+    }
+
+    private void empByDept(List<Employee> employees) {
+        Map<String, Long> emp = employees.stream()
+                .filter(employee -> "Sales And Marketing".equalsIgnoreCase(employee.getDepartment()))
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+        System.out.println("10.No. male and female employees are there in the sales and marketing team: " + emp);
+    }
+
+    private void maxWorkExp(List<Employee> employees) {
+        Employee emp = employees.stream()
+                .max(Comparator.comparingLong(employee -> {
+                    return ChronoUnit.YEARS.between(LocalDate.of(employee.getYearOfJoining(), 1, 1), LocalDate.now());
+                })).get();
+        System.out.println("9.Most working experience in the organization: " + emp);
+
+        Employee empOpt = employees.stream()
+                .min(Comparator.comparingInt(Employee::getYearOfJoining))
+                .get();
+        System.out.println("9.Most working experience in the organization empOpt: " + empOpt);
+    }
+
+    private void youngestMaleEmpByDept(List<Employee> employees) {
+        Employee emp = employees.stream()
+                .filter(employee -> "Product Development".equalsIgnoreCase(employee.getDepartment()))
+                .filter(employee -> "Male".equalsIgnoreCase(employee.getGender()))
+                .min(Comparator.comparingInt(Employee::getAge))
+                .get();
+        System.out.println("8.Details of youngest male employee in the product development department: " + emp);
+    }
+
+    private void avgSalByDept(List<Employee> employees) {
+        Map<String, Double> emp = employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(Employee::getSalary)));
+        System.out.println("7.Average salary of each department: " + emp);
+    }
+
+    private void countEmpDept(List<Employee> employees) {
+        Map<String, Long> emp = employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+        System.out.println("6.Number of employees in each department: " + emp);
+    }
+
+    private void employeesAfterYear(List<Employee> employees) {
+        List<String> emp = employees.stream()
+                .filter(employee -> employee.getYearOfJoining() > 2015)
+                .map(Employee::getName)
+                .toList();
+        System.out.println("5.Names of all employees who have joined after 2015: " + emp);
+    }
+
+    private void highestPaidEmployee(List<Employee> employees) {
+        Employee employee = employees.stream()
+                .max(Comparator.comparing(Employee::getSalary))
+                .get();
+        System.out.println("4.Highest paid employee in the organization: " + employee);
+
+        //optimized
+        Employee employeeOptimized = employees.stream()
+                .collect(Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)))
+                .get();
+        System.out.println("4.Highest paid employeeOptimized in the organization: " + employeeOptimized);
     }
 
     private void avgAgeOfEmployees(List<Employee> employees) {
@@ -33,14 +183,14 @@ public class EMSservice {
                 .forEach(entry -> {
                     int avg = (entry.getValue().stream()
                             .map(Employee::getAge)
-                            .reduce(0, (acc, x) -> acc + x))/entry.getValue().size();
-                    System.out.print("3."+entry.getKey() + " employees average age : " + avg+" , ");
+                            .reduce(0, (acc, x) -> acc + x)) / entry.getValue().size();
+                    System.out.print("3." + entry.getKey() + " employees average age : " + avg + " , ");
                 });
 
         //optimized
         Map<String, Double> map = employees.stream()
                 .collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingInt(Employee::getAge)));
-        System.out.println("\n3.Employees average age: "+map);
+        System.out.println("\n3.Employees average age: " + map);
     }
 
     private void printDepartments(List<Employee> employees) {
