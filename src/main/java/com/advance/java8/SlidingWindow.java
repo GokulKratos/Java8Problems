2b63afc5081d868bfa74fc179e18b07a7a07e62d
GokulKratos <99967107+GokulKratos@users.noreply.github.com>
2024-09-17 17:34:53 +0530

    Update SlidingWindow.java

--- src/main/java/com/advance/java8/SlidingWindow.java
+++ src/main/java/com/advance/java8/SlidingWindow.java
@@ -5,14 +5,45 @@ public class SlidingWindow {
 	public static void main(String[] args) {
 		SlidingWindow sw = new SlidingWindow();
 
-		//Maximum Points You Can Obtain from Cards
+		// Maximum Points You Can Obtain from Cards
 		int[] arr = { 6, 2, 3, 4, 7, 2, 1, 7, 1 };
 		int k = 4;
 		sw.maxCards(arr, k);
 
-		//Longest Substring Without Repeating Characters
+		// Longest Substring Without Repeating Characters
 		String str = "cadbzabcd";
 		sw.longestSubstring(str);
+
+		// Max Consecutive Ones III
+		int[] nums = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
+		sw.maxOnes(nums, 2);
+	}
+
+	private void maxOnes(int[] nums, int k) {
+		int l = 0;
+		int r = 0;
+		int max = 0;
+		int zeroes = 0;
+		int n = nums.length;
+
+		while (r < n) {
+			if (nums[r] == 0) {
+				zeroes++;
+			}
+
+			if (zeroes > k) {
+				if (nums[l] == 0) {
+					zeroes--;
+				}
+				l++;
+			}
+
+			if (zeroes <= k) {
+				max = Math.max(max, r - l + 1);
+			}
+			r++;
+		}
+		System.out.println("Max consecutive ones: " + max);
 	}
 
 	private void longestSubstring(String str) {
