dfc817473fcea8a51f5c2e673c60c2dcff50d66c
GokulKratos <99967107+GokulKratos@users.noreply.github.com>
2024-09-17 15:54:40 +0530

    Update SlidingWindow.java

--- src/main/java/com/advance/java8/SlidingWindow.java
+++ src/main/java/com/advance/java8/SlidingWindow.java
@@ -1,11 +1,37 @@
+import java.util.Arrays;
+
 public class SlidingWindow {
 
 	public static void main(String[] args) {
 		SlidingWindow sw = new SlidingWindow();
 
+		//Maximum Points You Can Obtain from Cards
 		int[] arr = { 6, 2, 3, 4, 7, 2, 1, 7, 1 };
 		int k = 4;
 		sw.maxCards(arr, k);
+
+		//Longest Substring Without Repeating Characters
+		String str = "cadbzabcd";
+		sw.longestSubstring(str);
+	}
+
+	private void longestSubstring(String str) {
+		int n = str.length();
+		int l = 0;
+		int r = 0;
+		int max = 0;
+		int[] hash = new int[256];
+		Arrays.fill(hash, -1);
+
+		while (r < n) {
+			if (hash[str.charAt(r)] != -1 && hash[str.charAt(r)] >= l) {
+				l = hash[str.charAt(r)] + 1;
+			}
+			hash[str.charAt(r)] = r;
+			max = Math.max(max, r - l + 1);
+			r++;
+		}
+		System.out.println("max: " + max);
 	}
 
 	private void maxCards(int[] arr, int k) {
